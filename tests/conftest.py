0c9b3459c8239a8c1a1419ad752289b626123216
Kostas <Ziovas>
2024-04-29 14:12:21 +0300

    fix: Test fixtures and create test app isntance

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,24 +2,42 @@
 
 import pytest
 
+from annotations_app import create_app
 from app.models.user import User
 from app.services.core_services import db
 
 
 @pytest.fixture(scope="module")
-def new_user():
-    user = User(
-        username="test_user", email="test@example.com", password_hash="password"
-    )
-    db.session.add(user)
-    db.session.commit()
-    yield user
-    db.session.delete(user)
-    db.session.commit()
+def app():
+    """Create and configure a new Flask application for testing."""
+    app = create_app()
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture(scope="module")
+def client(app):
+    """Create a test client for the Flask application."""
+    return app.test_client()
+
+
+@pytest.fixture(scope="module")
+def new_user(app):
+    """Create a new user and add it to the test database."""
+    with app.app_context():
+        user = User(
+            username="test_user", email="test@example.com", password_hash="password"
+        )
+        db.session.add(user)
+        db.session.commit()
+        yield user
+        db.session.delete(user)
+        db.session.commit()
 
 
 @pytest.fixture(scope="module")
 def new_user_data():
+    """Provide data for creating a new user."""
     return {
         "username": "test_user",
         "email": "test@example.com",
--- tests/unit/models/test_user.py
+++ tests/unit/models/test_user.py
@@ -1,16 +1,11 @@
 from uuid import UUID
 
-import pytest
-
-from app.models.user import User
-
-
 
 class TestUserModel:
     def test_user_creation(self, new_user):
         assert new_user.username == "test_user"
         assert new_user.email == "test@example.com"
-        assert new_user.password_hash == "hashed_password"
+        assert new_user.password_hash == "password"
 
     def test_user_representation(self, new_user):
         assert repr(new_user) == "<User test_user>"
